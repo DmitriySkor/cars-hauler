7c16e933e2648b23a193120b37943ec02eea6100
ValentinGor <v.gorbatenko@mail.ru>
2020-11-18 17:18:16 +0200

    Добавлена секция Our Blog

--- src/js/main.js
+++ src/js/main.js
@@ -17,28 +17,26 @@ document.getElementById("sub_menu_link-2").onclick = function (e) {
 $(document).ready(function () {
     let elems = $('.item').length;
 
-    if(elems >=5){
+    if (elems >= 5) {
         $(".carousel-reviews").owlCarousel({
-            loop:true,
-            margin:10,
-            nav:false,
-            responsive:{
-                0:{
-                    items:1
+            loop: true,
+            margin: 10,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1
                 },
-                600:{
-                    items:3
+                600: {
+                    items: 3
                 },
-                1000:{
-                    items:6
+                1000: {
+                    items: 6
                 }
             }
         });
     } else {
         $('.carousel-reviews').trigger('destroy.owl.carousel');
     }
-
-
 
 
 // Инициализация jQueryFormStyler
