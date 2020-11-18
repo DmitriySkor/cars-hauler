eb03b6c07be91aca14e6852eb1bd8b323b11c542
ValentinGor <v.gorbatenko@mail.ru>
2020-11-18 12:14:18 +0200

    Добавлена форма

--- src/js/main.js
+++ src/js/main.js
@@ -13,11 +13,11 @@ document.getElementById("sub_menu_link-2").onclick = function (e) {
     document.getElementById("sub_menu_link-ul-3").classList.toggle("active")
 }
 
-// Инициализация owlCarousel
-$(document).ready(function () {
-    let elems = $('.item').length;
 
-    if(elems >=6){
+$(document).ready(function () {
+    // Инициализация owlCarousel
+    let elems = $('.item').length;
+        if(elems >=6){
         $(".carousel-reviews").owlCarousel({
             loop:true,
             margin:10,
