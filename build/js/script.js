5141ce070f91295faf4bfef66ca18891ca8870ec
ValentinGor <v.gorbatenko@mail.ru>
2020-12-09 19:22:01 +0200

    Убрана ссылка на mail.php

--- build/js/script.js
+++ build/js/script.js
@@ -693,29 +693,6 @@ $(function () {
 //     console.log(inputDatepicker.value)
 // }
 
-
-$(document).ready(function () {
-
-    //E-mail Ajax Send
-    $("form").submit(function () { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function () {
-            alert("Thank you!");
-            setTimeout(function () {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
-
-});
-
-
 $(document).ready(
     function () {
         //Create a variable for the CarQuery object.  You can call it whatever you like.
